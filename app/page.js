d420dc7b5391bab172630c840c6f55367b6d0cb5
naufalF21 <naufalfirman098@gmail.com>
2024-02-07 02:09:22 +0700

    feat(Homepage): add Quotes section

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 'use client';
 
-import Card from '@/components/Card';
 import Header from '@/components/Header';
 import Jumbotron from '@/components/Jumbotron';
+import Quotes from '@/components/quotes';
 
 export default function Home() {
 	return (
@@ -11,7 +11,7 @@ export default function Home() {
 			<main>
 				<div className="max-w-[1024px] mx-auto ">
 					<Jumbotron />
-					<Card />
+					<Quotes />
 				</div>
 			</main>
 		</>
---
+++ components/quotes/index.js
@@ -0,0 +1,27 @@
+'use client';
+
+import { FaQuoteLeft } from 'react-icons/fa';
+import { FaQuoteRight } from 'react-icons/fa';
+
+export default function Quotes() {
+	return (
+		<div className="text-white mb-20 px-6">
+			<div className="relative top-4 left-6 flex justify-start">
+				<div className="bg-secondary z-10 px-2">
+					<FaQuoteLeft size="26" />
+				</div>
+			</div>
+			<div className="border py-8">
+				<h2 className="text-center text-lg font-semibold">
+					Programming isn&apos;t about what you know; it&apos;s about what you can figure
+					out. -Chris Pine
+				</h2>
+			</div>
+			<div className="flex justify-end relative bottom-4 right-6">
+				<div className="bg-secondary z-10 px-2">
+					<FaQuoteRight size="26" />
+				</div>
+			</div>
+		</div>
+	);
+}
