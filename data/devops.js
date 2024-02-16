b38a8de53707d0bae2adc92b5361c2e7ec88c661
naufalF21 <naufalfirman098@gmail.com>
2024-02-17 05:23:28 +0700

    style(Homepage): add github to devops data

--- data/devops.js
+++ data/devops.js
@@ -2,6 +2,7 @@ import { FaGitAlt } from 'react-icons/fa';
 import { SiJira } from 'react-icons/si';
 import { SiPostman } from 'react-icons/si';
 import { TbBrandVscode } from 'react-icons/tb';
+import { FaGithub } from 'react-icons/fa';
 
 const devops = [
 	{
@@ -23,6 +24,11 @@ const devops = [
 		icon: TbBrandVscode,
 		name: 'VSCode',
 		classname: 'text-[#0078d7]',
+	},
+	{
+		icon: FaGithub,
+		name: 'Github',
+		classname: '',
 	},
 ];
 
