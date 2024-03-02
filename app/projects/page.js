89eb6bec020e56087582a656ca2eee1f66920b25
naufalF21 <naufalfirman098@gmail.com>
2024-03-02 19:09:41 +0700

    style(Productpage): apply scroll animation

--- app/projects/page.js
+++ app/projects/page.js
@@ -2,22 +2,28 @@
 
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
+import Reveal from '@/components/Reveal';
 import ProjectsSection from '@/containers/projects-page/projects-section';
+import { LazyMotion, domAnimation } from 'framer-motion';
 
 export default function Projects() {
 	return (
-		<>
+		<LazyMotion features={domAnimation}>
 			<Header />
-			<main className="text-white max-w-[1024px] mx-auto pt-32 pb-16">
+			<main className="text-white max-w-[1024px] mx-auto pt-32 pb-16 px-4">
 				<div className="flex flex-col gap-2">
-					<h2 className="text-3xl font-semibold">
-						<span className="text-primary">/</span>projects
-					</h2>
-					<p className="text-gray">List of my project</p>
+					<Reveal width="fit-content">
+						<h2 className="text-3xl font-semibold">
+							<span className="text-primary">/</span>projects
+						</h2>
+					</Reveal>
+					<Reveal width="fit-content">
+						<p className="text-gray">List of my project</p>
+					</Reveal>
 				</div>
 				<ProjectsSection />
 			</main>
 			<Footer />
-		</>
+		</LazyMotion>
 	);
 }
--- containers/projects-page/projects-section/index.js
+++ containers/projects-page/projects-section/index.js
@@ -1,15 +1,18 @@
 'use client';
 
 import Card from '@/components/Card';
+import Reveal from '@/components/Reveal';
 import projects from '@/data/projects';
 
 export default function ProjectsSection() {
 	return (
 		<div className="mt-12 gap-12">
-			<div className="flex flex-row items-center gap-5 w-1/2">
-				<h2 className="text-3xl font-semibold">Big Enough</h2>
-				<hr className="text-primary border w-[50%]" />
-			</div>
+			<Reveal width="fit-content">
+				<div className="flex flex-row items-center gap-5 md:w-96">
+					<h2 className="text-3xl font-semibold">Big Enough</h2>
+					<hr className="text-primary border w-full hidden md:block" />
+				</div>
+			</Reveal>
 			<div className="flex flex-row flex-wrap justify-between py-10 gap-y-10">
 				{projects.map((item, index) => (
 					<Card
